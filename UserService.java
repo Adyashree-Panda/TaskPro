7a0c752cbb475ea80a752002803a4bcbd3205f63
Adyashree-Panda <135547066+Adyashree-Panda@users.noreply.github.com>
2025-06-01 01:40:00 +0530

    Create UserService.java

---
+++ UserService.java
@@ -0,0 +1,32 @@
+// UserService.java
+package com.taskmanager.service;
+
+import com.taskmanager.entity.User;
+import com.taskmanager.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public void updateUser(User user) {
+        userRepository.save(user);
+    }
+}
