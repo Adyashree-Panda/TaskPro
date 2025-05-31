e9a96aa03589c124bceced05950b50ab4b05867a
Adyashree-Panda <135547066+Adyashree-Panda@users.noreply.github.com>
2025-06-01 01:40:49 +0530

    Create TaskService.java

---
+++ TaskService.java
@@ -0,0 +1,33 @@
+// TaskService.java
+package com.taskmanager.service;
+
+import com.taskmanager.entity.Task;
+import com.taskmanager.entity.User;
+import com.taskmanager.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public Task saveTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public List<Task> getTasksByUser(User user) {
+        return taskRepository.findByUser(user);
+    }
+
+    public Task updateTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+}
