9a3155db1c6395df9683d270fe4bd9474e2e4805
Adyashree-Panda <135547066+Adyashree-Panda@users.noreply.github.com>
2025-06-01 13:35:18 +0530

    Update authReducers.js

--- authReducers.js
+++ authReducers.js
@@ -13,6 +13,14 @@ const authReducer = (state = initialState, action) => {
             return { ...state, loading: false, success: true, user: action.payload };
         case 'SIGNUP_FAILURE':
             return { ...state, loading: false, error: action.payload };
+
+            case 'LOGIN_REQUEST':
+    return { ...state, loading: true, error: null };
+case 'LOGIN_SUCCESS':
+    return { ...state, loading: false, user: action.payload };
+case 'LOGIN_FAILURE':
+    return { ...state, loading: false, error: action.payload };
+
         default:
             return state;
     }
