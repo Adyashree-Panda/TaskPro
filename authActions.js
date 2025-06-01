95ac589c693f53ef6f31908ece2d198ea117b751
Adyashree-Panda <135547066+Adyashree-Panda@users.noreply.github.com>
2025-06-01 13:33:10 +0530

    Update authActions.js

--- authActions.js
+++ authActions.js
@@ -1,1 +1,12 @@
 
+import axios from 'axios';
+
+export const signupUser = (userData) => async (dispatch) => {
+    dispatch({ type: 'SIGNUP_REQUEST' });
+    try {
+        const res = await axios.post('http://localhost:5000/api/auth/signup', userData);
+        dispatch({ type: 'SIGNUP_SUCCESS', payload: res.data });
+    } catch (error) {
+        dispatch({ type: 'SIGNUP_FAILURE', payload: error.response?.data || 'Signup failed' });
+    }
+};
