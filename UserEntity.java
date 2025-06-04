2f043f0e530e3738c9906f2cbaa39f871fe02211
Adyashree-Panda <135547066+Adyashree-Panda@users.noreply.github.com>
2025-06-04 08:35:57 +0530

    Update UserEntity.java

--- UserEntity.java
+++ UserEntity.java
@@ -1,43 +1,40 @@
-package com.taskmanager.entity;
-
-import jakarta.persistence.*;
-import lombok.*;
-
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @Table(name = "users")
 public class User {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String email;
+
+    private String password;
 
     private String username;
 
-    @Column(nullable = false, unique = true)
-    private String email;
-
-    @Column(nullable = false)
-    private String password;
-
-    private String profilePictureUrl;
+    private String profileImageUrl;
 
     @ManyToOne
     @JoinColumn(name = "question1_id")
     private SecurityQuestion question1;
 
+    private String answer1;
+
     @ManyToOne
     @JoinColumn(name = "question2_id")
     private SecurityQuestion question2;
+
+    private String answer2;
 
     @ManyToOne
     @JoinColumn(name = "question3_id")
     private SecurityQuestion question3;
 
-    private String answer1;
-    private String answer2;
     private String answer3;
+
+    // Add relationship with tasks
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Task> tasks;
+
+    // Getters and Setters...
 }
